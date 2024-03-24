bf1658a160c7515f0d2a9e07f56b49fb9eeb00ce
Danielius Zajenckauskas <dan@Danieliuss-MacBook-Pro.local>
2024-03-24 14:16:12 +0200

    header

--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@ const nextConfig = {
                 protocol: 'http',
                 hostname: '154.49.136.99',
                 pathname: '**',
+            },
+            {
+                protocol: 'http',
+                hostname: '154.49.136.99:1344',
+                pathname: '**',
             }
         ],
     },
