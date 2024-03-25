8e03bcb8f04445b86c16d9701e72831f77fb2c42
Danielius Zajenckauskas <dan@Danieliuss-MacBook-Pro.local>
2024-03-25 10:10:40 +0200

    button link

--- COMPONENTS/common/Header.tsx
+++ COMPONENTS/common/Header.tsx
@@ -86,25 +86,27 @@ export default function Header() {
                         display: 'flex', flexDirection: 'row', gap: 15,
                         alignItems: 'center',
                     }}>
-                        <Button variant='outlined' sx={{
-                            display: { md: 'flex', sm: 'flex', xs: 'none' },
-                            border: '1px solid #ccc',
-                            borderRadius: '3px', fontWeight: 700,
-                            fontSize: 14,
-                            padding: '14px 20px',
-                            alignItems: 'center',
-                            fontFamily: 'inherit',
-                            cursor: 'pointer'
-                        }}>
-                            <svg fill="#000000" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
-                                style={{ marginRight: 10 }} width="15" height="15" viewBox="0 0 569.16 569.16"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-                                <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" stroke="#CCCCCC" strokeWidth="25.043039999999998"></g>
-                                <g id="SVGRepo_iconCarrier"> <g> <g> <path d="M451.919,0h-122.4c-25.355,0-45.9,20.551-45.9,45.9s20.544,45.9,45.9,45.9h119.34V477.36h-119.34 c-25.355,0-45.9,20.545-45.9,45.899c0,25.355,20.544,45.9,45.9,45.9h122.4c48.93,0,88.74-39.811,88.74-88.74V88.74 C540.659,39.811,500.849,0,451.919,0z"></path>
-                                    <path d="M401.216,258.209L215.797,72.792c-14.566-14.566-26.371-9.676-26.371,10.924v70.777H43.421 c-8.238,0-14.92,6.677-14.92,14.921v230.332c0,8.237,6.683,14.921,14.92,14.921h146.005v70.777 c0,20.594,11.805,25.49,26.371,10.925l185.418-185.418C415.78,296.386,415.78,272.774,401.216,258.209z"></path> </g> </g> </g></svg>
-                            <Typography variant='h5' fontWeight={600} sx={{ display: { md: 'flex', xs: 'none' }, }}>
-                                CUSTOMER PORTAL
-                            </Typography>
-                        </Button>
+                        <Link passHref href={'https://admin.deliver1.co.uk/customerPortal/login'} >
+                            <Button variant='outlined' sx={{
+                                display: { md: 'flex', sm: 'flex', xs: 'none' },
+                                border: '1px solid #ccc',
+                                borderRadius: '3px', fontWeight: 700,
+                                fontSize: 14,
+                                padding: '14px 20px',
+                                alignItems: 'center',
+                                fontFamily: 'inherit',
+                                cursor: 'pointer'
+                            }}>
+                                <svg fill="#000000" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
+                                    style={{ marginRight: 10 }} width="15" height="15" viewBox="0 0 569.16 569.16"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                                    <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" stroke="#CCCCCC" strokeWidth="25.043039999999998"></g>
+                                    <g id="SVGRepo_iconCarrier"> <g> <g> <path d="M451.919,0h-122.4c-25.355,0-45.9,20.551-45.9,45.9s20.544,45.9,45.9,45.9h119.34V477.36h-119.34 c-25.355,0-45.9,20.545-45.9,45.899c0,25.355,20.544,45.9,45.9,45.9h122.4c48.93,0,88.74-39.811,88.74-88.74V88.74 C540.659,39.811,500.849,0,451.919,0z"></path>
+                                        <path d="M401.216,258.209L215.797,72.792c-14.566-14.566-26.371-9.676-26.371,10.924v70.777H43.421 c-8.238,0-14.92,6.677-14.92,14.921v230.332c0,8.237,6.683,14.921,14.92,14.921h146.005v70.777 c0,20.594,11.805,25.49,26.371,10.925l185.418-185.418C415.78,296.386,415.78,272.774,401.216,258.209z"></path> </g> </g> </g></svg>
+                                <Typography variant='h5' fontWeight={600} sx={{ display: { md: 'flex', xs: 'none' }, }}>
+                                    CUSTOMER PORTAL
+                                </Typography>
+                            </Button>
+                        </Link>
                         <Stack sx={{ display: { md: 'flex', xs: 'none' } }}>
                             <HeaderPinkElement />
                         </Stack>
