177ebeb84618d14ed40cafa2002f2e9c55d82a3e
Danielius Zajenckauskas <dan@Danieliuss-MacBook-Pro.local>
2024-03-28 10:38:25 +0200

    imgs

--- app/about-us/AboutUsPage.tsx
+++ app/about-us/AboutUsPage.tsx
@@ -10,19 +10,21 @@ import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
 import React, { useState } from 'react'
 import EastIcon from '@mui/icons-material/East';
+import Image from 'next/image'
+
 type Props = {
     articleContinents?: ContinentsResponseType;
 }
 const AboutUsPage = ({ articleContinents }: Props) => {
     const [active, setActive] = useState<string | undefined>('europe')
     const renderCountries = countries.filter((c) => c.continent && c.continent?.toLowerCase() === active)?.map((c) => {
-        const capitalizeEachWord = (str) => {
-            return str.replace(/\b\w/g, (char) => char.toUpperCase());
+        const capitalizeEachWord = (str: string) => {
+            return str.replace(/\b\w/g, (char: string) => char.toUpperCase());
         };
         return (
             <Stack key={c.country}>
-                <Typography>
-                    <EastIcon />  {capitalizeEachWord(c.country)}
+                <Typography variant='body1' sx={{ alignItems: 'center', display: 'flex', gap: 1 }}>
+                    <EastIcon fontSize='large' />     {capitalizeEachWord(c.country)}
                 </Typography>
             </Stack>
         )
@@ -110,16 +112,22 @@ const AboutUsPage = ({ articleContinents }: Props) => {
                         <Typography variant={'h1'} sx={{ pb: 2 }}>
                             About us
                         </Typography>
-                        <Stack direction={'row'}>
-                            <Stack sx={{ width: '50%' }}>
+                        <Stack direction={{ md: 'row', xs: 'column' }} spacing={4}>
+                            <Stack sx={{ width: '100%' }}>
                                 <Typography variant={'h2'} sx={{ pb: 1 }}>
                                     Exceptional moving services
                                 </Typography>
-                                <Typography sx={{ fontSize: 14 }}>
+                                <Typography variant='body1'>
                                     Deliver1 was founded by Charlie Beck and Robertas Gailaitis, who have been active in the industry since 2013. Through their extensive experience, they recognized a significant gap in the market: a scarcity of moving companies that successfully blend value for money with high-quality service. Motivated to fill this niche, Beck and Gailaitis established Deliver1 in early 2019. Their mission was clear - to offer clients exceptional moving services without compromising on affordability or quality. Since its inception, Deliver1 has been committed to redefining the moving industry by consistently delivering excellence in every aspect of its service.
                                 </Typography>
                             </Stack>
-                            <Stack sx={{ width: '50%' }}>
+                            <Stack sx={{ width: '100%', height: { md: 'auto', xs: 300 }, position: 'relative' }}>
+                                <Image
+                                    alt="backgorund"
+                                    src={"/box-img.png"}
+                                    objectFit='cover'
+                                    objectPosition='center'
+                                    fill />
                             </Stack>
                         </Stack>
                     </Stack>
@@ -127,8 +135,8 @@ const AboutUsPage = ({ articleContinents }: Props) => {
             </Stack>
             <Stack sx={{ backgroundColor: '#fff' }}>
                 <MaxWidthContainer sx={{ display: 'flex', flexDirection: 'column', py: 6 }}>
-                    <Stack spacing={10} direction={'row'} width={'100%'}>
-                        <Stack sx={{ width: '50%', }}>
+                    <Stack spacing={10} direction={{ md: 'row', xs: 'column' }} width={'100%'}>
+                        <Stack sx={{ width: '100%', }}>
                             <Typography variant={'h2'} sx={{ pb: 1 }}>
                                 Services we offer
                             </Typography>
@@ -136,23 +144,30 @@ const AboutUsPage = ({ articleContinents }: Props) => {
                                 {renderServices}
                             </Stack>
                         </Stack>
-                        <Stack sx={{ width: '50%' }}>
+                        <Stack sx={{ width: '100%' }}>
                             <Typography variant={'h2'} sx={{ pb: 2 }}>
                                 Other services we offer include:
                             </Typography>
                             <Stack spacing={1}>
-                                <Typography variant='body1'>
-                                    <EastIcon />     Storage solutions
+                                <Typography variant='body1' sx={{ alignItems: 'center', display: 'flex', gap: 1 }}>
+                                    <EastIcon fontSize='large' />     Storage solutions
                                 </Typography>
-                                <Typography variant='body1'>
-                                    <EastIcon />   Storage solutions
+                                <Typography variant='body1' sx={{ alignItems: 'center', display: 'flex', gap: 1 }}>
+                                    <EastIcon fontSize='large' />   Comprehensive professional packing
                                 </Typography>
-                                <Typography variant='body1'>
-                                    <EastIcon />   Storage solutions
+                                <Typography variant='body1' sx={{ alignItems: 'center', display: 'flex', gap: 1 }}>
+                                    <EastIcon fontSize='large' />   Specialized crating for delicate items
                                 </Typography>
-                                <Typography variant='body1'>
-                                    <EastIcon />   Storage solutions
+                                <Typography variant='body1' sx={{ alignItems: 'center', display: 'flex', gap: 1 }}>
+                                    <EastIcon fontSize='large' />   Furniture disassembly and assembly
                                 </Typography>
+                            </Stack>
+                            <Stack sx={{ width: '100%', height: { md: 300, xs: 300 }, position: 'relative', top: { md: 88, xs: 48 } }}>
+                                <Image
+                                    alt="backgorund"
+                                    src={"/sofa2cut.png"}
+                                    objectFit='contain'
+                                    fill />
                             </Stack>
                         </Stack>
                     </Stack>
