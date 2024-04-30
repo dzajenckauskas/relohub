8ab6c7263d0f3d0b284ca681f953b2be7c9092e9
Danielius Zajenckauskas <dan@Danieliuss-MacBook-Pro.local>
2024-04-30 10:50:33 +0300

    og img

--- app/page.tsx
+++ app/page.tsx
@@ -21,10 +21,10 @@ export async function generateMetadata({ }): Promise<Metadata> {
             canonical: process.env.NEXT_PUBLIC_DOMAIN_URL,
         },
         openGraph: {
-            images: [`${process.env.NEXT_PUBLIC_DOMAIN_URL}og-image.jpeg`]
+            images: [`${process.env.NEXT_PUBLIC_DOMAIN_URL}/og-image.jpeg`]
         },
         twitter: {
-            images: [`${process.env.NEXT_PUBLIC_DOMAIN_URL}og-image.jpeg`]
+            images: [`${process.env.NEXT_PUBLIC_DOMAIN_URL}/og-image.jpeg`]
         }
     }
 }
