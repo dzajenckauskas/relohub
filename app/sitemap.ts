dd296aa4eca5836c6d4035b69cfb5407a51b72e9
Danielius Zajenckauskas <dan@Danieliuss-MacBook-Pro.local>
2024-06-08 10:38:47 +0300

    sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,13 +1,23 @@
 import { ArticleDataType } from '@/COMPONENTS/types/ArticleTypes';
 import { CategoryDataType } from '@/COMPONENTS/types/CategoryTypes';
+import { CountryDataType } from '@/COMPONENTS/types/CountryType';
 import { getData } from '@/UTILS/getData'
 import { MetadataRoute } from 'next'
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     const domain = process.env.NEXT_PUBLIC_DOMAIN_URL
+    const countries = await getData(`${process.env.NEXT_PUBLIC_API_URL}/api/countries?pagination[limit]=100`)
     const categories = await getData(`${process.env.NEXT_PUBLIC_API_URL}/api/article-categories`)
     const articles = await getData(`${process.env.NEXT_PUBLIC_API_URL}/api/articles?populate=articleCategory`)
 
+    const renderCountries = countries?.data?.map((country: CountryDataType) => {
+        return {
+            url: `${domain}/moving-to/${country?.attributes?.url}`,
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.8,
+        }
+    })
     const renderCategories = categories?.data?.map((category: CategoryDataType) => {
         return {
             url: `${domain}/guides/${category?.attributes?.key}`,
@@ -49,7 +59,32 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
             changeFrequency: 'weekly',
             priority: 0.8,
         },
+        {
+            url: `${domain}/moving-within-europe`,
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.8,
+        },
+        {
+            url: `${domain}/international-moving`,
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.8,
+        },
+        {
+            url: `${domain}/relocation-services`,
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.8,
+        },
+        {
+            url: `${domain}/moving-services`,
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.8,
+        },
         ...renderCategories,
-        ...renderArticles
+        ...renderArticles,
+        ...renderCountries
     ]
 }
