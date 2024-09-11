94ef76bae0e22d866c8a4fdbd1b120bbfeb48903
Danielius Zajenckauskas <dan@Danieliuss-MacBook-Pro.local>
2024-09-11 08:11:05 +0300

    gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,28 +5,38 @@ import "/STYLES/medias.css";
 import Script from "next/script";
 
 export default function RootLayout({ children }) {
-    const gaId = 'GTM-MNL6VBLH'
+    const gaId = 'GTM-MNL6VBLH';
     return (
         <html lang="en">
             <head>
-                {gaId && <Script
-                    src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
-                    strategy="afterInteractive"
-                    rel={'preload'}
-                />}
-                {gaId && <Script id="google-analytics" strategy="afterInteractive" rel={'preload'}>
-                    {`
+                {/* Google Tag Manager */}
+                <Script id="gtm-script" strategy="afterInteractive">
+                    {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','${gaId}');`}
+                </Script>
+                {/* End Google Tag Manager */}
+
+                {gaId && (
+                    <Script
+                        src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
+                        strategy="afterInteractive"
+                        rel={'preload'}
+                    />
+                )}
+                {gaId && (
+                    <Script id="google-analytics" strategy="afterInteractive" rel={'preload'}>
+                        {`
                         window.dataLayer = window.dataLayer || [];
                         function gtag(){dataLayer.push(arguments);}
                         gtag('js', new Date());
                         gtag('config', '${gaId}');
                     `}
-                </Script>}
-                <link
-                    rel="preload"
-                    href="/logo2.png"
-                    as="image"
-                />
+                    </Script>
+                )}
+                <link rel="preload" href="/logo2.png" as="image" />
                 <link
                     rel="preload"
                     href="/fonts/Uniform-Condensed.ttf"
@@ -62,7 +72,6 @@ export default function RootLayout({ children }) {
                     type="font/ttf"
                     crossOrigin="anonymous"
                 />
-
                 <link
                     rel="apple-touch-icon"
                     sizes="180x180"
@@ -87,6 +96,16 @@ export default function RootLayout({ children }) {
                 />
             </head>
             <body>
+                {/* Google Tag Manager (noscript) */}
+                <noscript>
+                    <iframe
+                        src={`https://www.googletagmanager.com/ns.html?id=${gaId}`}
+                        height="0"
+                        width="0"
+                        style={{ display: 'none', visibility: 'hidden' }}
+                    ></iframe>
+                </noscript>
+                {/* End Google Tag Manager (noscript) */}
                 {children}
             </body>
         </html>
