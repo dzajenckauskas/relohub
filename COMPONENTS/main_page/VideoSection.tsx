66aa7d587759d8273d2c2d5cfca854337d37a8b3
Danielius Zajenckauskas <dan@Danieliuss-MacBook-Pro.local>
2024-09-17 11:50:42 +0300

    video section

---
+++ COMPONENTS/main_page/VideoSection.tsx
@@ -0,0 +1,75 @@
+"use client";
+import { useEffect, useRef, useState } from "react";
+import Image from "next/image";
+import { MaxWidthContainer } from "../common/MaxWidthContainer";
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
+import { VideSectionType } from "../types/CountryType";
+
+type Props = {
+    hideIcons?: boolean;
+    videoSection?: VideSectionType;
+}
+
+export default function VideoSection({ hideIcons, videoSection }: Props) {
+    const videoRef = useRef();
+    const slideelement = useRef(null);
+    const [showvideohide, setshowvideohide] = useState(true);
+    const [pixelRatio, setPixelRatio] = useState(1);
+    const [currentSlide, setCurrentSlide] = useState(0);
+
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+            setPixelRatio(window.devicePixelRatio || 1);
+        }
+    }, []);
+
+    useEffect(() => {
+        const elm = slideelement.current;
+        if (elm) {
+            elm.style.transform = `translateX(${currentSlide * -200}px)`;
+        }
+    }, [currentSlide, slideelement]);
+
+    function handleButtonClick() {
+        setshowvideohide(false);
+        if (videoRef?.current) {
+            (videoRef?.current as any)?.play();
+        }
+    }
+    return (
+        <Stack sx={{ backgroundColor: '#202020', pt: hideIcons ? 6 : 0, pb: hideIcons ? 3 : 0 }}>
+            <MaxWidthContainer>
+                <section className="videoareaglobalwrp">
+                    <div className="videoareavideowrp">
+                        {showvideohide ? (
+                            <div className="videoareaplaybtnwrp">
+                                <button
+                                    className="videopresentationtroundbutton"
+                                    onClick={handleButtonClick}
+                                >
+                                    <Image
+                                        src={"/play.svg"}
+                                        style={{ objectFit: "contain" }}
+                                        className="playsvgbtn"
+                                        width={40}
+                                        height={40}
+                                        alt="play button"
+                                    ></Image>
+                                </button>
+                            </div>
+                        ) : null}
+
+                        {videoSection?.video?.data?.attributes?.url &&
+                            <video
+                                ref={videoRef}
+                                className="videoelm"
+                                src={`${process.env.NEXT_PUBLIC_API_URL}${videoSection?.video?.data?.attributes?.url}`}
+                                controls
+                            ></video>}
+                    </div>
+                </section>
+            </MaxWidthContainer>
+        </Stack>
+    );
+}
