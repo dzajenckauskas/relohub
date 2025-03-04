fc01c2924b26a0e32c52026e9049982a430d7721
Danielius Zajenckauskas <dan@Danieliuss-MacBook-Pro.local>
2025-03-04 22:17:43 +0200

    phone input fix

--- app/offer-new/FormStyledPhoneInput.tsx
+++ app/offer-new/FormStyledPhoneInput.tsx
@@ -1,15 +1,21 @@
-import { ArrowDropDown } from "@mui/icons-material";
+'use client'
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import CloseIcon from '@mui/icons-material/Close';
 import {
-    Box,
     InputAdornment,
-    MenuItem,
-    Select,
-    TextField
+    Popper,
+    TextField,
+    Typography
 } from "@mui/material";
-import { CountryCode, getCountries, getCountryCallingCode } from "libphonenumber-js";
-import { Controller } from "react-hook-form";
+import Autocomplete from '@mui/material/Autocomplete';
+import Stack from '@mui/material/Stack';
+import { styled } from '@mui/system';
 import i18nCountries from "i18n-iso-countries";
 import enLocale from "i18n-iso-countries/langs/en.json"; // Load English names
+import { CountryCode, getCountries, getCountryCallingCode } from "libphonenumber-js";
+import Image from "next/image";
+import { useState } from 'react';
+import { Controller } from "react-hook-form";
 i18nCountries.registerLocale(enLocale);
 
 const countryList = getCountries()
@@ -34,17 +40,44 @@ type Props = {
 };
 
 const FormStyledPhoneInput = ({ form, name, label, required, disabled, fullWidth, error, helperText }: Props) => {
-    const handleCountryChange = (e: any) => {
-        const newCountry = e.target.value;
-        const countryData = countryList.find(c => c.code === (newCountry ?? 'GB'));
+    const [open, setOpen] = useState(false);
+    const [selectedCountry, setSelectedCountry] = useState({
+        code: 'GB', dialCode: '+44', flag: "https://flagcdn.com/w40/gb.png", name: ''
+    });
+    const GroupHeader = styled('div')(({ theme }) => ({
+        padding: '4px 14px',
+        marginTop: 8,
+        marginBottom: 8,
+        textAlign: 'center',
+        width: 56,
+        marginLeft: 16,
+        borderRadius: 5,
+        color: '#fff',
+        fontWeight: 'bold',
+        backgroundColor: theme.palette.secondary.main
+    }));
 
-        if (countryData) {
-            // setSelectedCountry(newCountry);
-            form.setValue("dialCode", countryData.dialCode); // ✅ Set dialCode instead of country code
-            form.setValue("countryCode", countryData.code); // ✅ Set dialCode instead of country code
-        }
+    const GroupItems = styled('ul')({
+        padding: 0,
+    });
+
+    const CustomPopper = (props) => {
+        return (
+            <Popper
+                {...props}
+                placement='bottom-start'
+                sx={{
+                    width: '240px !important',
+                    marginLeft: '-14px !important', // Fine-tune the positioning, if needed
+
+                }}
+            />
+        );
+    }
+    const filterOptions = (options, state) => {
+        state.inputValue.toLowerCase()
+        return options?.filter((v) => v.name?.toLowerCase()?.includes(state.inputValue.toLowerCase()) || v.code?.toLowerCase()?.includes(state.inputValue.toLowerCase()) || v.dialCode?.toLowerCase()?.includes(state.inputValue.toLowerCase()))
     };
-
     return (
         <Controller
             name={name}
@@ -85,42 +118,92 @@ const FormStyledPhoneInput = ({ form, name, label, required, disabled, fullWidth
                     InputProps={{
                         startAdornment: (
                             <InputAdornment position="start">
-                                <Select
-                                    value={form.watch('countryCode') ?? 'GB'}
-                                    onChange={handleCountryChange}
-                                    renderValue={(selected) => {
-                                        const selectedCountry = countryList.find(c => c.code === selected);
-                                        return selectedCountry ? (
-                                            <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
-                                                <img src={selectedCountry.flag} alt={selectedCountry.code} width={20} height={15} />
-                                                {selectedCountry.dialCode}
-                                            </Box>
-                                        ) : "+44"; // ✅ Show flag + dial code when selected
+                                <Autocomplete
+                                    disabled={disabled}
+                                    open={open}
+                                    sx={{ width: 120 }}
+                                    PopperComponent={CustomPopper}
+                                    filterOptions={filterOptions}
+                                    popupIcon={<ArrowDropDownIcon sx={{ fontSize: 30 }} />}
+                                    clearIcon={<CloseIcon sx={{ fontSize: 20 }} />}
+                                    disablePortal
+                                    value={selectedCountry}
+                                    getOptionLabel={(option) => `${option.dialCode || ''}`}
+                                    id="combo-box-demo"
+                                    options={
+                                        countryList?.map((v) => v) ??
+                                        []}
+                                    groupBy={(option) => option?.name?.[0]}
+                                    renderInput={(params) => (
+                                        <TextField
+                                            {...params}
+                                            InputProps={{
+                                                ...params.InputProps,
+                                                sx: {
+                                                    minHeight: 50,
+                                                    backgroundColor: "#efefef",
+                                                    borderRadius: 1,
+                                                    color: "black",
+                                                    "& .MuiOutlinedInput-notchedOutline": { border: "none" }
+                                                },
+                                                startAdornment: (
+                                                    <InputAdornment position="start" sx={{ position: 'relative', left: -9 }}>
+                                                        <Image
+                                                            loading="lazy"
+                                                            width={20}
+                                                            height={15}
+                                                            style={{ objectFit: "contain" }}
+                                                            src={selectedCountry?.flag}
+                                                            alt={selectedCountry?.code}
+                                                        />
+                                                    </InputAdornment>)
+                                            }}
+                                            name={name}
+                                            InputLabelProps={{
+                                                shrink: true,
+                                                sx: { fontSize: "2.4rem", fontWeight: 400, color: "black", pb: 2, top: -12, left: -8 }
+                                            }}
+                                        />
+                                    )}
+                                    renderGroup={(params) => (
+                                        <li key={params.key}>
+                                            <GroupHeader>{params.group}</GroupHeader>
+                                            <GroupItems>{params.children}</GroupItems>
+                                        </li>
+                                    )}
+                                    disableClearable
+                                    onOpen={() => setOpen(true)}
+                                    onClose={() => setOpen(false)}
+                                    onChange={(_e, v) => {
+                                        setSelectedCountry(v)
+                                        form.setValue("dialCode", v.dialCode); // ✅ Set dialCode instead of country code
+                                        form.setValue("countryCode", v.code); // ✅ Set dialCode instead of country code
+                                        return v
                                     }}
-                                    sx={{
-                                        display: "flex",
-                                        alignItems: "center",
-                                        minWidth: "40px !important",
-                                        width: 100,
-                                        borderRadius: "5px",
-                                        position: 'relative',
-                                        right: 13,
-                                        '.MuiSelect-iconOutlined': {
-                                            transform: 'scale(2)'
-                                        },
+                                    renderOption={(props, option) => {
+                                        return (
+                                            <li {...props} key={option?.code} style={{ borderBottom: '1px solid #EBEBEB' }}>
+                                                <Stack sx={{ width: '100%', cursor: 'pointer', margin: '4px auto' }}>
+                                                    <Stack direction={'row'} alignItems={'flex-start'} spacing={1} sx={{ width: '94%' }}>
+                                                        <Image
+                                                            loading="lazy"
+                                                            width={24}
+                                                            height={16}
+                                                            style={{ objectFit: "contain" }}
+                                                            src={option?.flag}
+                                                            alt={option?.code}
+                                                        />
+                                                        <Typography sx={{
+                                                            maxWidth: 220,
+                                                            textWrap: 'wrap',
+                                                            lineHeight: 1.1
+                                                        }}><b>{option?.dialCode}</b> {option?.name}</Typography>
+                                                    </Stack>
+                                                </Stack>
+                                            </li>
+                                        )
                                     }}
-                                    IconComponent={ArrowDropDown}
-                                >
-                                    {countryList.map(({ code, name, dialCode, flag }) => (
-                                        <MenuItem key={code} value={code}>
-                                            <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
-                                                <img src={flag} alt={code} width={20} height={15} />
-                                                <b style={{ fontWeight: 500 }}>{dialCode}</b> {name}
-                                            </Box>
-                                        </MenuItem>
-                                    ))}
-                                </Select>
-
+                                />
 
                             </InputAdornment>
                         )
